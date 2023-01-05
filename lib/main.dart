cc1a6341f21468157a95f02927791b31326efe07
reisalazar <rrsb45@gmail.com>
2023-01-05 18:17:27 -0300

    formating date

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'dart:ui';
+
 import 'models/transaction.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 void main() => runApp(ExpensesApp());
 
@@ -72,7 +75,7 @@ class MyHomePage extends StatelessWidget {
                                   ),
                                 ),
                                 Text(
-                                  tr.date.toString(),
+                                  DateFormat('d MMM y').format(tr.date),
                                   style: TextStyle(
                                     color: Colors.grey,
                                   ),
