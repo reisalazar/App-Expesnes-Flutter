34eb7aa3421f9b1cf035009be33f7e0dee741e0b
reisalazar <rrsb45@gmail.com>
2023-01-12 19:07:41 -0300

    landscape screen 2

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -11,23 +11,28 @@ class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return transactions.isEmpty
-        ? Column(
-            children: [
-              const SizedBox(height: 20),
-              Text(
-                "Nenhuma transação cadastrada!",
-                style: Theme.of(context).textTheme.headline6,
-              ),
-              const SizedBox(height: 20),
-              SizedBox(
-                height: 200,
-                child: Image.asset(
-                  'assets/images/waiting.png',
-                  fit: BoxFit.cover,
+        ? LayoutBuilder(builder: (context, constraints) {
+            return Column(
+              children: [
+                SizedBox(height: constraints.maxHeight * 0.05),
+                Container(
+                  height: constraints.maxHeight * 0.3,
+                  child: Text(
+                    "Nenhuma transação cadastrada!",
+                    style: Theme.of(context).textTheme.headline6,
+                  ),
                 ),
-              ),
-            ],
-          )
+                SizedBox(height: constraints.maxHeight * 0.05),
+                SizedBox(
+                  height: constraints.maxHeight * 0.6,
+                  child: Image.asset(
+                    'assets/images/waiting.png',
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ],
+            );
+          })
         : ListView.builder(
             itemCount: transactions.length,
             itemBuilder: (context, index) {
